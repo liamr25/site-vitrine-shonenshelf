8c177b3ac3a63efa156257f92abd9d09795dc4b3
unknown <ramirez-montesl@SIOPERGAUD.FR>
2026-01-27 12:06:24 +0100

    premier commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: ["./**/*.html", "./**/*.js"],
+    theme: {
+        extend: {
+            colors: {
+                "shonen-red": "#dc2626",
+                "shonen-dark": "#0f172a",
+                "shonen-black": "#020617",
+                "shonen-gray": "#1e293b",
+            },
+            fontFamily: {
+                headings: ['"Russo One"', "sans-serif"],
+                body: ['"Roboto"', "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
